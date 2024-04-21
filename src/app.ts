e7116bbd6ed628ebca3522c46da29f20f6f0c92a
Tito Carlos <titocarlos080@gmail.com>
2024-04-21 13:19:21 -0400

    correginedo errores

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
-// app.ts
 import express from "express";
 import http from "http";
 import path from "path";
 import { Server as SocketIOServer } from "socket.io";
 import { PanelRuta } from "./sockets/panelSocketServer";
-import {Rutas  } from "./routes"
+import { Rutas } from "./routes";
+
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(Rutas); 
 const server = http.createServer(app);
 const io = new SocketIOServer(server);
 
-// Configurar servidor Socket.IO para la ruta /Panel
+// Configura el middleware para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+// Middleware para manejar solicitudes JSON y URL codificadas
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Configura la carpeta de vistas y el motor de plantillas EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// Agrega las rutas definidas en el archivo 'routes.ts'
+app.use(Rutas);
+
+// Configura el servidor Socket.IO para la ruta '/Panel'
 PanelRuta.configuracionPanelSocket(io);
 
 const PORT = process.env.PORT || 3000;
